7dc678bedd62409105edb1a8f0c7ea350ef46859
Lars <laarrss@protonmail.com>
2025-05-01 13:08:12 +0200

    auth filer tilføjet

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import staysRoutes from "./routes/stay.route.js";
 import activitiesRouter from "./routes/activity.route.js";
 import reviewRouter from "./routes/review.route.js";
 import userRouter from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 
 // Server
 const expressServer = express();
@@ -22,6 +23,7 @@ expressServer.use(staysRoutes);
 expressServer.use(activitiesRouter);
 expressServer.use(reviewRouter);
 expressServer.use(userRouter);
+expressServer.use(authRoute);
 
 expressServer.listen(3042, () => {
   console.log("Serveren kører på http://localhost:3042");
